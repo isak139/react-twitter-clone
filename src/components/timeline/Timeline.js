a022a2ae7c6c3e3ca261ccefc0e383f5c1c8e1ee
isak <t-yuta08780126@outlook.jp>
2022-12-04 16:30:42 +0900

    Update Timeline.js

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Timeline.css";
+import TweetBox from "./TweetBox";
 
 function Timeline() {
   return (
-    <div className="timeline_header">
-      <h2>ホーム</h2>
+    <div className="timeline">
+      <div className="timeline_header">
+        <h2>ホーム</h2>
+      </div>
+      <TweetBox />
     </div>
   );
 }
