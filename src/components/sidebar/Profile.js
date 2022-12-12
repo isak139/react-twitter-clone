210273d43bf80489c06effa52ec36ea410054e49
isak <t-yuta08780126@outlook.jp>
2022-12-12 14:15:51 +0900

    Update Profile.js

--- src/components/sidebar/Profile.js
+++ src/components/sidebar/Profile.js
@@ -1,16 +1,20 @@
-import React from "react";
+//import React from "react";
+import { useContext } from "react";
 import { Avatar } from "@mui/material";
 import "./Profile.css";
+import { AuthContext } from "../../AuthContext";
 
-function Profile({ displayName, userName, userIcon }) {
+function Profile() {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className="Profile">
-      <Avatar src={userIcon} />
-      <div className="Profile_body">
-        <h3>{displayName}</h3>
-        <p>@{userName}</p>
+    currentUser && (
+      <div className="Profile">
+        <Avatar src={currentUser.photoURL} />
+        <div className="Profile_body">
+          <h3>{currentUser.displayName}</h3>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
