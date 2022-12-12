da5b5d1208ded368fcd7730f94b084684b9abd6f
isak <t-yuta08780126@outlook.jp>
2022-12-12 14:15:57 +0900

    Update Timeline.js

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -5,7 +5,7 @@ import TweetBox from "./TweetBox";
 import { db } from "../../firebase";
 import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 
-function Timeline({ isAuth }) {
+function Timeline() {
   const [posts, setPosts] = React.useState([]);
 
   React.useEffect(() => {
@@ -21,7 +21,7 @@ function Timeline({ isAuth }) {
       <div className="timeline_header">
         <h2>ホーム</h2>
       </div>
-      <TweetBox isAuth={isAuth} />
+      <TweetBox />
       {posts.map((post) => (
         <Post
           key={post.text /* UUIDなどに変更すべき */}
