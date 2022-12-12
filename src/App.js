716b6353893347c2d75732a26ab3063d05a89dfb
isak <t-yuta08780126@outlook.jp>
2022-12-12 14:15:46 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import Widgets from "./components/widget/Widgets";
 import Sidebar from "./components/sidebar/Sidebar";
 import Timeline from "./components/timeline/Timeline";
-import { useState } from "react";
+import { AuthProvider } from "./AuthContext";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+  //const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
   return (
     <div className="app">
-      <Sidebar isAuth={isAuth} setIsAuth={setIsAuth} />
-      <Timeline isAuth={isAuth} />
-      <Widgets />
+      <AuthProvider>
+        <Sidebar /* isAuth={isAuth} setIsAuth={setIsAuth} */ />
+        <Timeline /* isAuth={isAuth} */ />
+        <Widgets />
+      </AuthProvider>
     </div>
   );
 }
