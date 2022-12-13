a6577a3627214c6957b1baf8ab460e066019bd09
isak <t-yuta08780126@outlook.jp>
2022-12-13 14:46:34 +0900

    Update Post.js

--- src/components/timeline/Post.js
+++ src/components/timeline/Post.js
@@ -2,35 +2,48 @@ import {
   FavoriteBorder,
   Publish,
   Repeat,
-  VerifiedUser,
+  //VerifiedUser,
   ChatBubbleOutline,
 } from "@mui/icons-material";
+import CloseIcon from "@mui/icons-material/Close";
 import { Avatar } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
 import "./Post.css";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { AuthContext } from "../../AuthContext";
 
-function Post({ displayName, username, verified, text, image, avatar }) {
+function Post({ post }) {
+  const { currentUser } = useContext(AuthContext);
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+  };
+
   return (
     <div className="post">
       <div className="post_avatar">
-        <Avatar src={avatar} />
+        <Avatar src={post.avatar} />
       </div>
       <div className="post_body">
         <div className="post_header">
           <div className="post_headerText">
-            <h3>
-              {displayName}
-              <span className="post_headerSpecial">
-                {verified ? <VerifiedUser className="post_badge" /> : ""}@
-                {username}
-              </span>
-            </h3>
+            <h3>{post.displayName}</h3>
+            {currentUser
+              ? currentUser.uid === post.uid && (
+                  <div
+                    className="post_closeIcon"
+                    onClick={() => handleDelete(post.docId)}
+                  >
+                    <CloseIcon />
+                  </div>
+                )
+              : null}
           </div>
           <div className="post_headerDescription">
-            <p>{text}</p>
+            <p>{post.text}</p>
           </div>
         </div>
-        <img src={image} alt="" />
+        <img src={post.image} alt="" />
         <div className="post_footer">
           <ChatBubbleOutline fontSize="small" />
           <Repeat fontSize="small" />
